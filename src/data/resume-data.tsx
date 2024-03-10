9f3e305ef8c4bf42956119243c4f96152fbff5c6
chris <chris61678@gmail.com>
2024-03-10 12:27:09 +1100

    first commit - updating content

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,88 +20,68 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Christopher Sung",
+  initials: "CS",
+  location: "Sydney AU",
+  locationLink: "https://www.google.com/maps/place/Sydney",
   about:
     "Full Stack Engineer focused on building products with extra attention to detail",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  personalWebsiteUrl: "https://christophersung.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "christopher.y.sung@gmail.com",
+    tel: "+61402211465",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/okyesgoodnice",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/cywsung/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Macquarie University",
+      degree: "Bachelor of Commerce",
+      start: "2018",
+      end: "2021",
+    },
+    {
+      school: "Western Sydney University",
+      degree: "Bachelor of Medicine/Bachelor of Surgery (MBBS)",
+      start: "2015",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "Freelance Developer",
+      link: "https://christophersung.dev",
+      badges: [],
+      title: "Web Developer",
+      logo: ClevertechLogo,
+      start: "2023",
+      end: "Present",
+      description:
+        "Building, styling and shipping tailored websites for a diverse array of clients. Technologies: HTML, CSS, JavaScript, TypeScript, React, NextJS, Ruby on Rails",
+    },
+    {
+      company: "Intuition",
+      link: "https://intuitioneducation.com.au",
+      badges: [],
+      title: "Full Stack Developer",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "Sep 2022",
+      end: "Dec 2023",
       description:
         "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
     },
   ],
   skills: [
